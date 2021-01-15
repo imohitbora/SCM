7d7d607c9e5472cb65edcd0af5f438dcbd0b12d6
mohitbora <mohit.bora@nagarro.com>
2021-01-15 11:50:49 +0530

    Added te test Project and nugets

---
+++ SCM.Console/SCM.Service.Test/OrderServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using SCM.Service.IService;
+using Xunit;
+
+namespace SCM.Service.Test
+{
+    public class OrderServiceTest
+    {
+        private readonly Mock<IPromotion> _promotionMock;
+
+        public OrderServiceTest()
+        {
+            _promotionMock = new Mock<IPromotion>();
+        }
+
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
